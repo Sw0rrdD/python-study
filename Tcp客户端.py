82bc14541333e490244131948f1a778c56f1ef78
zhoucj <zhoucj93@hotmail.com>
2015-10-07 10:12:27 +0800

    Tcp服务器.py

--- Tcp客户端.py
+++ Tcp客户端.py
@@ -1,17 +1,14 @@
 import socket
 
-target_host = "www.baidu.com"
-target_port = 80
+target_host = "127.0.0.1"
+target_port = 9999
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 client.connect((target_host,target_port))
 
-<<<<<<< HEAD
 client.send("GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n") 
-=======
-client.send("GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n")
->>>>>>> a187d5e012d50fc02b6694a6f55a8c4468934192
+
 
 response = client.recv(4096)
 
